df2ad7fc32ea9cdd1543e25deb519d83b2c9bd78
Kate Ramsey <kateramsey@live.com>
2016-03-21 10:35:25 -0500

    All tests pass

---
+++ InheritanceChallenge/UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InheritanceChallenge
+{
+    public class Vehicle
+    {
+        public Vehicle(string make, string model)
+        {
+            Make = make;
+            Model = model;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int NumOfGears { get; set; } = 4;
+        public int NumOfTires { get; set; } = 4;
+        public override string ToString()
+        {
+            return $"This is a {Make} {Model}";
+        }
+    }
+
+
+
+
+
+
+    [TestClass]
+    public class InheritanceChallengeTests
+    {
+        [TestMethod]
+        public void Classes_Inherit()
+        {
+            Assert.IsTrue(typeof(ElectricCar).IsSubclassOf(typeof(Vehicle)));
+            Assert.IsTrue(typeof(Motorcycle).IsSubclassOf(typeof(Vehicle)));
+        }
+
+        [TestMethod]
+        public void Vehicles_Describe_Themselves()
+        {
+            var truck = new ElectricCar("Nissan", "Leaf");
+            Assert.AreEqual("This is a Nissan Leaf", truck.ToString());
+
+            var motorcycle = new Motorcycle("Honda", "CTX700N");
+            Assert.AreEqual("This is a Honda CTX700N", motorcycle.ToString());
+        }
+
+
+        [TestMethod]
+        public void Correct_Number_Of_Gears()
+        {
+            var truck = new ElectricCar("Nissan", "Leaf");
+            Assert.AreEqual(1, truck.NumOfGears);
+
+            var motorcycle = new Motorcycle("Honda", "CTX700N");
+            Assert.AreEqual(4, motorcycle.NumOfGears);
+        }
+
+        [TestMethod]
+        public void Correct_Number_Of_Tires()
+        {
+            var truck = new ElectricCar("Nissan", "Leaf");
+            Assert.AreEqual(4, truck.NumOfTires);
+
+            var motorcycle = new Motorcycle("Honda", "CTX700N");
+            Assert.AreEqual(2, motorcycle.NumOfTires);
+        }
+
+
+        [TestMethod]
+        public void Trike_Creation()
+        {
+            Assert.IsTrue(typeof(Trike).IsSubclassOf(typeof(Motorcycle)));
+
+            var trike = new Trike("Can-Am", "Spyder");
+            Assert.AreEqual(3, trike.NumOfTires);
+        }
+    }
+
+    public class Trike : Motorcycle
+    {
+        private string v1;
+        private string v2;
+
+        public Trike(string v1, string v2) : base(v1, v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            NumOfTires = 3;
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle(string make, string model) : base(make, model)
+        {
+            NumOfTires = 2;
+        }
+    }
+
+    public class ElectricCar : Vehicle
+    {
+        public ElectricCar(string make, string model) : base(make, model)
+        {
+            NumOfGears = 1;
+        }
+    }
+}
